5d07d3defb3d97c074a7772d1a112a0e401a8d57
007AxelBerkati <bucel1916@gmail.com>
2022-10-01 14:44:03 +0700

    add callback and update formating date

--- src/components/MoodPicker.tsx
+++ src/components/MoodPicker.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState, useCallback } from 'react';
+import { View, Text, StyleSheet, Pressable } from 'react-native';
+import { theme } from '../theme';
+import { MoodOptionType } from '../type';
 
-const moodOptions = [
+const moodOptions: MoodOptionType[] = [
   { emoji: '🧑‍💻', description: 'studious' },
   { emoji: '🤔', description: 'pensive' },
   { emoji: '😊', description: 'happy' },
@@ -9,19 +11,68 @@ const moodOptions = [
   { emoji: '😤', description: 'frustrated' },
 ];
 
-export const MoodPicker: React.FC = () => {
+type MoodPickerProps = {
+  onSelect: (mood: MoodOptionType) => void;
+};
+
+export const MoodPicker: React.FC<MoodPickerProps> = ({ onSelect }) => {
+  const [selectedMood, setSelectedMood] = useState<MoodOptionType>();
+
+  const handleSelect = useCallback(() => {
+    if (selectedMood) {
+      onSelect(selectedMood);
+      setSelectedMood(undefined);
+    }
+  }, [onSelect, selectedMood]);
+
   return (
-    <View style={styles.moodList}>
-      {moodOptions.map(option => (
-        <Text key={option.emoji} style={styles.moodText}>
-          {option.emoji}
-        </Text>
-      ))}
+    <View style={styles.container}>
+      <Text style={styles.heading}>How Are You Right Now?</Text>
+      <View style={styles.moodList}>
+        {moodOptions.map(option => (
+          <View>
+            <Pressable
+              onPress={() => setSelectedMood(option)}
+              key={option.emoji}
+              style={[
+                styles.moodItem,
+                option.emoji === selectedMood?.emoji
+                  ? styles.selectedMoodItem
+                  : undefined,
+              ]}>
+              <Text style={styles.emoji}>{option.emoji}</Text>
+            </Pressable>
+            <Text style={styles.descriptionText}>
+              {selectedMood?.emoji === option.emoji ? option.description : ''}
+            </Text>
+          </View>
+        ))}
+      </View>
+      <Pressable onPress={handleSelect} style={styles.button}>
+        <Text style={styles.buttonText}>Choose</Text>
+      </Pressable>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  container: {
+    padding: 16,
+    backgroundColor: theme.colorWhite,
+    borderWidth: 2,
+    borderColor: theme.colorPurple,
+    borderRadius: 10,
+    margin: 10,
+  },
+
+  heading: {
+    fontSize: 20,
+    letterSpacing: 1,
+    color: theme.colorPurple,
+    textAlign: 'center',
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
   moodText: {
     fontSize: 24,
     color: 'black',
@@ -29,6 +80,43 @@ const styles = StyleSheet.create({
   moodList: {
     flexDirection: 'row',
     justifyContent: 'space-between',
-    padding: 20,
+  },
+
+  moodItem: {
+    width: 60,
+    height: 60,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 30,
+    marginBottom: 5,
+  },
+
+  selectedMoodItem: {
+    borderWidth: 2,
+    backgroundColor: '#454C73',
+    borderColor: '#fff',
+  },
+
+  descriptionText: {
+    fontSize: 10,
+    color: '#454C73',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  button: {
+    backgroundColor: theme.colorPurple,
+    alignSelf: 'center',
+    paddingHorizontal: 40,
+    paddingVertical: 10,
+    borderRadius: 8,
+    marginTop: 16,
+  },
+  buttonText: {
+    color: theme.colorWhite,
+    fontSize: 16,
+  },
+
+  emoji: {
+    fontSize: 20,
   },
 });
--- src/screens/Home.screen.tsx
+++ src/screens/Home.screen.tsx
@@ -1,11 +1,22 @@
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import React, { useState, useCallback } from 'react';
+import { StyleSheet, View, Text } from 'react-native';
 import { MoodPicker } from '../components/MoodPicker';
+import { MoodOptionType, MoodOptionWithTimestamp } from '../type';
+import { MoodItemRow } from '../components/MoodItemRow';
 
 export const Home: React.FC = () => {
+  const [moodList, setMoodList] = useState<MoodOptionWithTimestamp[]>([]);
+
+  const handleSelectedMood = useCallback((mood: MoodOptionType) => {
+    setMoodList(curent => [...curent, { mood, timestamp: Date.now() }]);
+  }, []);
+
   return (
     <View style={styles.container}>
-      <MoodPicker />
+      <MoodPicker onSelect={handleSelectedMood} />
+      {moodList.map(item => (
+        <MoodItemRow key={item.timestamp} item={item} />
+      ))}
     </View>
   );
 };
