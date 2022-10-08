4badbfd65a14c49022266243a476e58611efcb23
007AxelBerkati <bucel1916@gmail.com>
2022-10-08 09:56:23 +0700

    Animation and gesture(not working)

--- android/app/src/main/java/com/moodtracker/MainActivity.java
+++ android/app/src/main/java/com/moodtracker/MainActivity.java
@@ -4,8 +4,21 @@ import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
 import android.os.Bundle;
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.ReactRootView;
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 
 public class MainActivity extends ReactActivity {
+
+  // @Override
+  // protected ReactActivityDelegate createReactActivityDelegate() {
+  //   return new ReactActivityDelegate(this, getMainComponentName()) {
+  //     @Override
+  //     protected ReactRootView createRootView() {
+  //      return new RNGestureHandlerEnabledRootView(MainActivity.this);
+  //     }
+  //   };
+  // }
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +33,9 @@ public class MainActivity extends ReactActivity {
   protected String getMainComponentName() {
     return "MoodTracker";
   }
+
+  
+
 
   /**
    * Returns the instance of the {@link ReactActivityDelegate}. There the RootView is created and
