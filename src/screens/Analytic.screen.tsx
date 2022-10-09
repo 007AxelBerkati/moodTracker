d3585f3f7ac09d149e21a0b3535935b3c5bdc983
007AxelBerkati <bucel1916@gmail.com>
2022-10-09 17:22:30 +0700

    add data visualization

--- src/screens/Analytic.screen.tsx
+++ src/screens/Analytic.screen.tsx
@@ -1,10 +1,22 @@
+import { groupBy } from 'lodash';
 import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
+import { useAppContext } from '../App.provider';
+import { VictoryPie } from 'victory-native';
 
 export const Analytic: React.FC = () => {
+  const appContext = useAppContext();
+
+  const data = Object.entries(groupBy(appContext.moodList, 'mood.emoji')).map(
+    ([key, value]) => ({
+      x: key,
+      y: value.length,
+    }),
+  );
+
   return (
     <View style={styles.container}>
-      <Text>Analytic</Text>
+      <VictoryPie data={data} />
     </View>
   );
 };
